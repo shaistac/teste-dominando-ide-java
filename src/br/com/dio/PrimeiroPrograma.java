fdf0cce7a1e4380e39f62256bfb56ae666e6326d
shaistac <shaistacamara@gmail.com>
2022-07-27 03:14:20 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
